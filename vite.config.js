b91aeb6b27f1bec7285bee2667f790c1d7820f3a
sothearaaura <chhoeuysothearaaura@gmail.com>
2025-10-01 17:14:47 +0700

    first commit

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: process.env.NODE_ENV === 'production' && process.env.VERCEL !== '1' 
+    ? '/sample/'  // For GitHub Pages
+    : '/',        // For Vercel and development
 })
